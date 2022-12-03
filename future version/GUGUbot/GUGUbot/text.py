e27bfe22e79e184e5124091211049bd8e63e5833
XueK <956481807@qq.com>
2022-12-03 16:50:54 -0500

    update partial version

--- future version/GUGUbot/GUGUbot/text.py
+++ future version/GUGUbot/GUGUbot/text.py
@@ -51,76 +51,76 @@ DEFAULT_CONFIG = {
 }
 
 group_help_msg = '''命令帮助如下:
-/list 获取在线玩家列表
-/flist 获取在线假人列表
-/bound <游戏ID> 绑定你的游戏ID
-/mc <消息> 向游戏内发送消息（可以触发游戏内关键词）
+#列表 获取在线玩家列表
+#假人 获取在线假人列表
+#绑定 <游戏ID> 绑定你的游戏ID
+#mc <消息> 向游戏内发送消息（可以触发游戏内关键词）
 关键词相关：
-/addg <关键词> <回复> 添加游戏内关键词回复
-/addp <关键词> 添加关键词图片
-/delg <关键词> 删除游戏内关键词
-/klistg 显示现有游戏内关键词列表
-/key_word 查看关键词相关帮助
-/style 机器人风格帮助'''
+#添加 <关键词> <回复> 添加游戏内关键词回复
+#添加图片 <关键词> 添加关键词图片
+#删除 <关键词> 删除关键词
+#游戏关键词 列表 显示现有游戏内关键词列表
+帮助 查看关键词相关帮助
+#风格 机器人风格帮助'''
 
 admin_help_msg = '''管理员命令帮助如下
-/bound 查看绑定相关帮助
-/whitelist 查看白名单相关帮助
-/start_command 查看启动指令相关帮助
-/ban_word 查看违禁词相关帮助
-/key_word 查看关键词相关帮助
-/ingame_key_word 查看游戏内关键词相关帮助
+#绑定 查看绑定相关帮助
+#白名单 查看白名单相关帮助
+#启动指令 查看启动指令相关帮助
+#违禁词 查看违禁词相关帮助
+#关键词 查看关键词相关帮助
+#游戏内关键词 查看游戏内关键词相关帮助
 /uuid 查看uuid 匹配相关帮助
-/name 查看机器人名字相关帮助
-（被关了）/command <command> 执行任意指令'''
+#名字 查看机器人名字相关帮助
+'''
 
-bound_help = '''/bound list 查看绑定列表
-/bound check <QQ号> 查询绑定ID
-/bound unbound <QQ号> 解除绑定
-/bound <QQ号> <游戏ID> 绑定新ID'''
+bound_help = '''#绑定 列表 查看绑定列表
+#绑定 查询 <QQ号> 查询绑定ID
+#绑定 解绑 <QQ号> 解除绑定
+#绑定 <QQ号> <游戏ID> 绑定新ID'''
 
-whitelist_help = '''/whitelist add <target> 添加白名单成员
-/whitelist list 列出白名单成员
-/whitelist off 关闭白名单
-/whitelist on 开启白名单
-/whitelist reload 重载白名单
-/whitelist remove <target> 删除白名单成员
+whitelist_help = '''#白名单 add <target> 添加白名单成员
+#白名单 列表 列出白名单成员
+#白名单 关 关闭白名单
+#白名单 开 开启白名单
+#白名单 重载 重载白名单
+#白名单 删除 <target> 删除白名单成员
 <target> 可以是玩家名/目标选择器/UUID'''
 
 start_command_help = '''启动指令菜单：
-/start_command add <名称> <指令> 添加启动指令
-/start_command list 查看现有启动指令
-/start_command del <名称> 删除指定启动指令
-/start_command on 开启开服指令
-/start_command off 关闭开服指令
-/start_command exe 执行一遍开服指令
-/start_command reload 重载开服指令''' 
+#启动指令 添加 <名称> <指令> 添加启动指令
+#启动指令 列表 查看现有启动指令
+#启动指令 删除 <名称> 删除指定启动指令
+#启动指令 开 开启开服指令
+#启动指令 关 关闭开服指令
+#启动指令 执行 执行一遍开服指令
+#启动指令 重载 重载开服指令''' 
 
 ban_word_help = '''违禁词相关指令：
-/ban_word add <违禁词> <违禁理由> 添加违禁词
-/ban_word list 显示违禁词表列及理由
-/ban_word del <违禁词> 删除指定违禁词
-/ban_word on 开启违禁词
-/ban_word off 关闭违禁词
-/ban_word reload 重载违禁词'''
+#违禁词 添加 <违禁词> <违禁理由> 添加违禁词
+#违禁词 列表 显示违禁词列表及理由
+#违禁词 删除 <违禁词> 删除指定违禁词
+#违禁词 开 开启违禁词
+#违禁词 关 关闭违禁词
+#违禁词 重载 重载违禁词'''
 
 key_word_help = '''关键词相关指令：
-/key_word on 开启关键词
-/key_word off 关闭关键词
-/key_word reload 重载关键词
-/klist 显示关键词列表
-/add <关键词> <回复> 添加关键词
-/del <关键词> 删除指定关键词'''
+#关键词 开 开启关键词
+#关键词 关 关闭关键词
+#关键词 重载 重载关键词
+#关键词 列表 显示关键词列表
+#添加 <关键词> <回复> 添加关键词
+#删除 <关键词> 删除指定关键词'''
 
 ingame_key_word_help = '''游戏内关键词相关指令：
-/ingame_key_word on 开启游戏内关键词
-/ingame_key_word off 关闭游戏内关键词
-/ingame_key_word reload 重载游戏内关键词'''
+#游戏关键词 开 开启游戏内关键词
+#游戏关键词 关 关闭游戏内关键词
+#游戏关键词 重载 重载游戏内关键词'''
 
 style_help = '''风格指令如下：
-/style        风格帮助
-/style list   风格列表
-/style <风格> 切换至指定风格'''
+#风格        风格帮助
+#风格 列表   风格列表
+#风格 <风格> 切换至指定风格'''
 
 uuid_help ='''uuid匹配指令如下：
 /uuid 查看uuid相关帮助
@@ -130,10 +130,10 @@ uuid_help ='''uuid匹配指令如下：
 /uuid name 查看白名单名字'''
 
 name_help = '''机器人名字相关指令如下：
-/name 查看名字相关帮助
-/name on 机器人名字显示为在线人数
-/name off 机器人名字为特殊空白名字
-(一直显示会占用少许服务器资源)
+#名字 查看名字相关帮助
+#名字 开 机器人名字显示为在线人数
+#名字 关 机器人名字为特殊空白名字
+(会占用少许服务器资源)
 '''
 
 shenhe_help = '''审核名单帮助：
@@ -153,7 +153,13 @@ style = {
         'server_start':'服务器已启动',
         'add_success':'添加成功！',
         'delete_success':'删除成功！',
-        'key_word_exist': '已有指定关键词,请删除(/del <关键词>)后重试 awa'
+        'key_word_exist': '已有指定关键词,请删除(/del <关键词>)后重试 awa',
+        'lack_parameter': '缺少参数，请参考 #帮助 里的说明',
+        'del_no_exist': '该关键词不存在',
+        'no_word': '列表空空的',
+        'list': '列表如下: \n{}',
+        'reload_success': '重载成功',
+        'command_success' : '指令执行成功'
     },
     '傲娇': {
         'ban_word_find':"本大小姐不听，才不告诉你是因为 {}",
@@ -163,6 +169,12 @@ style = {
         'server_start': '服务器启动啦！笨、笨蛋，人家可不是特地来告诉你们的!',
         'add_success':'哼..既然你都这么说了,那我就勉为其难帮你添加了！',
         'delete_success':'删除完了，才...才不是特别为你做的呢!',
-        'key_word_exist': '已有指定关键词,请删除(/del <关键词>)后重试 awa'
+        'key_word_exist': '已有指定关键词,请删除(/del <关键词>)后重试 awa',
+        'lack_parameter': '笨死了，这都不会1w1，去看 #帮助 啦！',
+        'del_no_exist': '累死我了，找了半天没这个词，大坏蛋！',
+        'no_word': '快添点东西进来叭，才。。才不是人家寂寞了',
+        'list': '列表如下: \n{}',
+        'reload_success': '',
+        'command_success' : '那么多指令，想累死本小姐嘛，才不做！什么，你说你准备了礼物，那我就勉为其难做点，哼'
     }
 }
