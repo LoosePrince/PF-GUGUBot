a7cba8bf7cc454535d2da13a13fec59b588d218e
XueK66 <74094966+XueK66@users.noreply.github.com>
2025-10-05 18:53:38 -0700

    [v2.0.0]Information Source

---
+++ GUGUbot/gugubot/parser/InfoSource/basicConstructor.py
@@ -0,0 +1,20 @@
+import json
+
+
+class BasicConstructor:
+    def __init__(self, raw_data: str | dict):
+        if isinstance(raw_data, str):
+            raw_data = json.loads(raw_data)
+        self.raw_data = raw_data
+
+    def __getattr__(self, key):
+        value = self.raw_data.get(key)
+        if value is None or isinstance(value, dict):
+            return BasicConstructor(value or {})
+        return value
+    
+    def __bool__(self):
+        return bool(self.raw_data)
+    
+    def __repr__(self):
+        return str(self.raw_data)
